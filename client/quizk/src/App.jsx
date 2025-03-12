157e94ac74b42d5f273265e9b463d3e7d558e01b
Ritish chhabra <ritishchhabra0786@gmail.com>
2025-03-12 15:51:42 +0530

    kkk

--- client/quizk/src/App.jsx
+++ client/quizk/src/App.jsx
@@ -2,21 +2,51 @@ import { useState,useEffect } from 'react'
 import axios from 'axios'
 
 
-function App() {
+function App() { 
   const [data, setdata] = useState([])
+  const [useranswers, setuseranswers] = useState({})
+  
+  const handleclick = (quid, option)=>{
+    setuseranswers(prev=>({
+      ...prev,
+
+      [quid] : option,
+
+
+
+    }));
+  };
+
+  const anscheck = ()=>{
+   data.forEach(ques => {
+    const select = useranswers[ques._id];
+
+    if (select===ques.correctAnswer) {
+      console.log("the ques is", ques.question,  " the answ is ", select, " right");
+    }
+    else{
+      console.log("the ques is", ques.question,  " the answ is ", select, " wrong");
+    }
+   });
+  }
   useEffect(() => {
     
     axios.get("http://localhost:3000/quiz").then((res)=>{
       setdata(res.data);
 
-      console.log(res.data);
     }).catch((err)=>{
       console.log( "There is a error",err);
     })
     
   }, [])
-  
 
+ useEffect(() => {
+ console.log(useranswers);
+ }, [useranswers])
+ 
+
+
+  
   return (
     <>
      <div className='bg-purple-800 text-white pl-5 '>
@@ -28,8 +58,14 @@ function App() {
             {
               ques.options.map((option, index )=>(
                   <li className='flex gap-3.5' key={index}>
-                    <input type='checkbox'/>
+                    <input type='radio' name = {ques._id}
+                     value={option}
+                     checked ={useranswers[ques._id]===option}
+                     onChange={()=>handleclick(ques._id, option)}
+                    
+                    />
                     <label> {option}</label>
+                    
                   
                   </li>
               ))
@@ -40,6 +76,9 @@ function App() {
 
   ))}
   </ul>
+
+<button onClick={anscheck}>click</button>
+
      </div>
     </>
   )
